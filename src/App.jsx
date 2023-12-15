959c9cb31a3c131639cff8636f59b32857e1c5a5
MaheshRautrao <101188065+MaheshRautrao@users.noreply.github.com>
2023-12-15 15:24:09 +0530

    api setup and testing done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,19 @@
+import { useEffect } from "react";
+import { fetchDataFromTMDB } from "./utils/api.js";
+
 function App() {
+  useEffect(() => {
+    apiTesting();
+  }, []);
+
+  const apiTesting = () => {
+    console.log(
+      fetchDataFromTMDB(
+        "/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc"
+      )
+    );
+  };
+
   return <>mahesh</>;
 }
 
