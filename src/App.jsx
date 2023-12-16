9add260970ceeacc90cde8c5ed90dfd29c4e3561
MaheshRautrao <101188065+MaheshRautrao@users.noreply.github.com>
2023-12-16 23:15:22 +0530

    redux store setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
 import { useEffect } from "react";
 import { fetchDataFromTMDB } from "./utils/api.js";
+import { useSelector, useDispatch } from "react-redux";
+import { getApiConfiguration } from "./store/homeSlice.js";
 
 function App() {
+  const state = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  console.log(state);
   useEffect(() => {
     apiTesting();
   }, []);
 
   const apiTesting = () => {
-    console.log(
-      fetchDataFromTMDB(
-        "/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc"
-      )
+    const data = fetchDataFromTMDB(
+      "/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc"
     );
+
+    // dispatch(getApiConfiguration(data));
   };
 
-  return <>mahesh</>;
+  return <></>;
 }
 
 export default App;
