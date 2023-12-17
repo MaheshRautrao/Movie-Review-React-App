2d917231a223be2cf7ceff30f0bc597e5ef3c241
MaheshRautrao <101188065+MaheshRautrao@users.noreply.github.com>
2023-12-17 23:32:46 +0530

    react-router setup and minor sections created

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import { useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { fetchDataFromTMDB } from "./utils/api.js";
 import { useSelector, useDispatch } from "react-redux";
 import { getApiConfiguration } from "./store/homeSlice.js";
+
+import Header from "./components/header/Header.jsx";
+import Footer from "./components/footer/Footer.jsx";
+import Home from "./pages/home/Home.jsx";
+import Details from "./pages/details/Details.jsx";
+import SearchResult from "./pages/searchResult/SearchResult.jsx";
+import Explore from "./pages/explore/Explore.jsx";
+import PageNotFound from "./pages/404/PageNotFound.jsx";
 
 function App() {
   const state = useSelector((state) => state);
@@ -20,7 +29,21 @@ function App() {
     // dispatch(getApiConfiguration(data));
   };
 
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/:mediaType/:id" element={<Details />} />
+          <Route path="/search/:query" element={<SearchResult />} />
+          <Route path="/explore/:mediaType" element={<Explore />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
